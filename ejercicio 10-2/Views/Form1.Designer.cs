a0ab1e348dbfb3a7376bd41a9447da9b4083c794
AlvaroAltavista <alvaroaltavista@gmail.com>
2025-05-08 15:07:47 +0200

    Vista Inicial
    Elementos de la vista inicial del programa

--- ejercicio 10-2/Views/Form1.Designer.cs
+++ ejercicio 10-2/Views/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ejercicio_10_2
 {
-    partial class Form1
+    partial class FormProfesores
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,309 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            grpDatos = new GroupBox();
+            txtEmail = new TextBox();
+            txtTelefono = new TextBox();
+            lblEmail = new Label();
+            lblTelefono = new Label();
+            txtApellidos = new TextBox();
+            lblApellidos = new Label();
+            txtNombre = new TextBox();
+            lblNombre = new Label();
+            txtDni = new TextBox();
+            lblDni = new Label();
+            lblPosicion = new Label();
+            grpNavegacion = new GroupBox();
+            btnUltimo = new Button();
+            btnAnterior = new Button();
+            btnSiguiente = new Button();
+            btnPrimero = new Button();
+            grpNuevoRegistro = new GroupBox();
+            grpGestionarRegistros = new GroupBox();
+            btnEliminarRegistro = new Button();
+            btnActualizarRegistro = new Button();
+            btnGuardarRegistro = new Button();
+            btnAnyadirRegistro = new Button();
+            grpDatos.SuspendLayout();
+            grpNavegacion.SuspendLayout();
+            grpNuevoRegistro.SuspendLayout();
+            grpGestionarRegistros.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grpDatos
+            // 
+            grpDatos.Controls.Add(txtEmail);
+            grpDatos.Controls.Add(txtTelefono);
+            grpDatos.Controls.Add(lblEmail);
+            grpDatos.Controls.Add(lblTelefono);
+            grpDatos.Controls.Add(txtApellidos);
+            grpDatos.Controls.Add(lblApellidos);
+            grpDatos.Controls.Add(txtNombre);
+            grpDatos.Controls.Add(lblNombre);
+            grpDatos.Controls.Add(txtDni);
+            grpDatos.Controls.Add(lblDni);
+            grpDatos.Location = new Point(45, 103);
+            grpDatos.Name = "grpDatos";
+            grpDatos.Size = new Size(953, 269);
+            grpDatos.TabIndex = 0;
+            grpDatos.TabStop = false;
+            grpDatos.Text = "Datos";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(585, 188);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(296, 34);
+            txtEmail.TabIndex = 5;
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(138, 188);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(296, 34);
+            txtTelefono.TabIndex = 3;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(515, 191);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(66, 31);
+            lblEmail.TabIndex = 0;
+            lblEmail.Text = "Email:";
+            lblEmail.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // lblTelefono
+            // 
+            lblTelefono.AutoSize = true;
+            lblTelefono.Location = new Point(39, 191);
+            lblTelefono.Name = "lblTelefono";
+            lblTelefono.Size = new Size(96, 31);
+            lblTelefono.TabIndex = 0;
+            lblTelefono.Text = "Teléfono:";
+            lblTelefono.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // txtApellidos
+            // 
+            txtApellidos.Location = new Point(585, 124);
+            txtApellidos.Name = "txtApellidos";
+            txtApellidos.Size = new Size(296, 34);
+            txtApellidos.TabIndex = 4;
+            // 
+            // lblApellidos
+            // 
+            lblApellidos.AutoSize = true;
+            lblApellidos.Location = new Point(483, 127);
+            lblApellidos.Name = "lblApellidos";
+            lblApellidos.Size = new Size(98, 31);
+            lblApellidos.TabIndex = 0;
+            lblApellidos.Text = "Apellidos:";
+            lblApellidos.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(138, 124);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(296, 34);
+            txtNombre.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(43, 127);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(92, 31);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre:";
+            lblNombre.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // txtDni
+            // 
+            txtDni.Location = new Point(138, 63);
+            txtDni.Name = "txtDni";
+            txtDni.Size = new Size(296, 34);
+            txtDni.TabIndex = 1;
+            // 
+            // lblDni
+            // 
+            lblDni.AutoSize = true;
+            lblDni.Location = new Point(87, 66);
+            lblDni.Name = "lblDni";
+            lblDni.RightToLeft = RightToLeft.No;
+            lblDni.Size = new Size(48, 31);
+            lblDni.TabIndex = 0;
+            lblDni.Text = "DNI:";
+            lblDni.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // lblPosicion
+            // 
+            lblPosicion.AutoSize = true;
+            lblPosicion.Font = new Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblPosicion.Location = new Point(45, 51);
+            lblPosicion.Name = "lblPosicion";
+            lblPosicion.Size = new Size(206, 42);
+            lblPosicion.TabIndex = 1;
+            lblPosicion.Text = "Posición actual";
+            // 
+            // grpNavegacion
+            // 
+            grpNavegacion.Controls.Add(btnUltimo);
+            grpNavegacion.Controls.Add(btnAnterior);
+            grpNavegacion.Controls.Add(btnSiguiente);
+            grpNavegacion.Controls.Add(btnPrimero);
+            grpNavegacion.Location = new Point(45, 421);
+            grpNavegacion.Name = "grpNavegacion";
+            grpNavegacion.Size = new Size(953, 142);
+            grpNavegacion.TabIndex = 2;
+            grpNavegacion.TabStop = false;
+            grpNavegacion.Text = "Navegación";
+            // 
+            // btnUltimo
+            // 
+            btnUltimo.Location = new Point(716, 57);
+            btnUltimo.Name = "btnUltimo";
+            btnUltimo.Size = new Size(210, 52);
+            btnUltimo.TabIndex = 0;
+            btnUltimo.Text = "Último";
+            btnUltimo.UseVisualStyleBackColor = true;
+            btnUltimo.Click += btnUltimo_Click;
+            // 
+            // btnAnterior
+            // 
+            btnAnterior.Location = new Point(485, 57);
+            btnAnterior.Name = "btnAnterior";
+            btnAnterior.Size = new Size(210, 52);
+            btnAnterior.TabIndex = 0;
+            btnAnterior.Text = "Anterior";
+            btnAnterior.UseVisualStyleBackColor = true;
+            btnAnterior.Click += btnAnterior_Click;
+            // 
+            // btnSiguiente
+            // 
+            btnSiguiente.Location = new Point(254, 57);
+            btnSiguiente.Name = "btnSiguiente";
+            btnSiguiente.Size = new Size(210, 52);
+            btnSiguiente.TabIndex = 0;
+            btnSiguiente.Text = "Siguiente";
+            btnSiguiente.UseVisualStyleBackColor = true;
+            btnSiguiente.Click += btnSiguiente_Click;
+            // 
+            // btnPrimero
+            // 
+            btnPrimero.Location = new Point(23, 57);
+            btnPrimero.Name = "btnPrimero";
+            btnPrimero.Size = new Size(210, 52);
+            btnPrimero.TabIndex = 0;
+            btnPrimero.Text = "Primero";
+            btnPrimero.UseVisualStyleBackColor = true;
+            btnPrimero.Click += btnPrimero_Click;
+            // 
+            // grpNuevoRegistro
+            // 
+            grpNuevoRegistro.Controls.Add(grpGestionarRegistros);
+            grpNuevoRegistro.Controls.Add(btnGuardarRegistro);
+            grpNuevoRegistro.Controls.Add(btnAnyadirRegistro);
+            grpNuevoRegistro.Location = new Point(45, 600);
+            grpNuevoRegistro.Name = "grpNuevoRegistro";
+            grpNuevoRegistro.Size = new Size(953, 142);
+            grpNuevoRegistro.TabIndex = 3;
+            grpNuevoRegistro.TabStop = false;
+            grpNuevoRegistro.Text = "Nuevo Registro";
+            // 
+            // grpGestionarRegistros
+            // 
+            grpGestionarRegistros.Controls.Add(btnEliminarRegistro);
+            grpGestionarRegistros.Controls.Add(btnActualizarRegistro);
+            grpGestionarRegistros.Location = new Point(473, 0);
+            grpGestionarRegistros.Name = "grpGestionarRegistros";
+            grpGestionarRegistros.Size = new Size(480, 142);
+            grpGestionarRegistros.TabIndex = 3;
+            grpGestionarRegistros.TabStop = false;
+            grpGestionarRegistros.Text = "Gestionar Registros";
+            // 
+            // btnEliminarRegistro
+            // 
+            btnEliminarRegistro.Location = new Point(243, 58);
+            btnEliminarRegistro.Name = "btnEliminarRegistro";
+            btnEliminarRegistro.Size = new Size(210, 52);
+            btnEliminarRegistro.TabIndex = 0;
+            btnEliminarRegistro.Text = "Eliminar";
+            btnEliminarRegistro.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizarRegistro
+            // 
+            btnActualizarRegistro.Location = new Point(12, 58);
+            btnActualizarRegistro.Name = "btnActualizarRegistro";
+            btnActualizarRegistro.Size = new Size(210, 52);
+            btnActualizarRegistro.TabIndex = 0;
+            btnActualizarRegistro.Text = "Actualizar";
+            btnActualizarRegistro.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardarRegistro
+            // 
+            btnGuardarRegistro.Location = new Point(254, 58);
+            btnGuardarRegistro.Name = "btnGuardarRegistro";
+            btnGuardarRegistro.Size = new Size(210, 52);
+            btnGuardarRegistro.TabIndex = 0;
+            btnGuardarRegistro.Text = "Guardar";
+            btnGuardarRegistro.UseVisualStyleBackColor = true;
+            // 
+            // btnAnyadirRegistro
+            // 
+            btnAnyadirRegistro.Location = new Point(23, 58);
+            btnAnyadirRegistro.Name = "btnAnyadirRegistro";
+            btnAnyadirRegistro.Size = new Size(210, 52);
+            btnAnyadirRegistro.TabIndex = 0;
+            btnAnyadirRegistro.Text = "Añadir";
+            btnAnyadirRegistro.UseVisualStyleBackColor = true;
+            // 
+            // FormProfesores
+            // 
+            AutoScaleDimensions = new SizeF(11F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1050, 805);
+            Controls.Add(grpNuevoRegistro);
+            Controls.Add(grpNavegacion);
+            Controls.Add(lblPosicion);
+            Controls.Add(grpDatos);
+            Font = new Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "FormProfesores";
+            Text = "Profesores";
+            Load += FormProfesores_Load;
+            grpDatos.ResumeLayout(false);
+            grpDatos.PerformLayout();
+            grpNavegacion.ResumeLayout(false);
+            grpNuevoRegistro.ResumeLayout(false);
+            grpGestionarRegistros.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox grpDatos;
+        private TextBox txtEmail;
+        private TextBox txtTelefono;
+        private Label lblEmail;
+        private Label lblTelefono;
+        private TextBox txtApellidos;
+        private Label lblApellidos;
+        private TextBox txtNombre;
+        private Label lblNombre;
+        private TextBox txtDni;
+        private Label lblDni;
+        private Label lblPosicion;
+        private GroupBox grpNavegacion;
+        private Button btnUltimo;
+        private Button btnAnterior;
+        private Button btnSiguiente;
+        private Button btnPrimero;
+        private GroupBox grpNuevoRegistro;
+        private Button btnGuardarRegistro;
+        private Button btnAnyadirRegistro;
+        private GroupBox grpGestionarRegistros;
+        private Button btnEliminarRegistro;
+        private Button btnActualizarRegistro;
     }
 }
--- ejercicio 10-2/Views/Form1.cs
+++ ejercicio 10-2/Views/Form1.cs
@@ -1,10 +1,105 @@
+using ejercicio_10_2.Models;
+
 namespace ejercicio_10_2
 {
-    public partial class Form1 : Form
+    public partial class FormProfesores : Form
     {
-        public Form1()
+        public FormProfesores()
         {
             InitializeComponent();
+        }
+
+        // MIEMBROS -------------------------------------
+        // Instancia del objeto de control de BD
+        SqlDBHelper sqlDBHelper;
+
+        private int posicion;
+
+        // LOAD -------------------------------------------------------
+        private void FormProfesores_Load(object sender, EventArgs e)
+        {
+            sqlDBHelper = new SqlDBHelper();
+
+            posicion = 0;
+            MostrarRegistro(posicion);
+            ControlBotonesNavegacion(posicion);
+
+            // Otros botones
+            btnGuardarRegistro.Enabled = false;
+            btnActualizarRegistro.Enabled = false;
+        }
+
+        // MÉTODOS ----------------------------------------------------
+
+        // Método que muestra el registro en los textBox
+        private void MostrarRegistro(int posicion)
+        {
+            Profesor profesor;
+
+            profesor = sqlDBHelper.DevolverProfesor(posicion);
+
+            txtDni.Text = profesor.Dni;
+            txtNombre.Text = profesor.Nombre;
+            txtApellidos.Text = profesor.Apellidos;
+            txtTelefono.Text = profesor.Tlf;
+            txtEmail.Text = profesor.Email;
+
+            lblPosicion.Text = $"Profesor {posicion + 1} de {sqlDBHelper.NumeroProfesores}";
+        }
+
+        // Método para controlar la activación de los botones de navegación
+        private void ControlBotonesNavegacion(int posicion)
+        {
+            if (posicion == 0)
+            {
+                btnPrimero.Enabled = false;
+                btnSiguiente.Enabled = true;
+                btnAnterior.Enabled = false;
+                btnUltimo.Enabled = true;
+            }
+            else if (posicion == sqlDBHelper.NumeroProfesores - 1)
+            {
+                btnPrimero.Enabled = true;
+                btnSiguiente.Enabled = false;
+                btnAnterior.Enabled = true;
+                btnUltimo.Enabled = false;
+            }
+            else
+            {
+                btnPrimero.Enabled = true;
+                btnSiguiente.Enabled = true;
+                btnAnterior.Enabled = true;
+                btnUltimo.Enabled = true;
+            }
+        }
+
+        // BOTONES ------------------------------------------------------------
+        private void btnPrimero_Click(object sender, EventArgs e)
+        {
+            posicion = 0;
+            MostrarRegistro(posicion);
+            ControlBotonesNavegacion(posicion);
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            posicion++;
+            MostrarRegistro(posicion);
+            ControlBotonesNavegacion(posicion);
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            posicion--;
+            MostrarRegistro(posicion);
+            ControlBotonesNavegacion(posicion);
+        }
+
+        private void btnUltimo_Click(object sender, EventArgs e)
+        {
+            posicion = sqlDBHelper.NumeroProfesores - 1;
+            MostrarRegistro(posicion);
+            ControlBotonesNavegacion(posicion);
         }
     }
 }
